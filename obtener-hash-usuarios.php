7d15ac695401f234a4e2887f35840670fdd600d2
fer <fernandolagonovoa@gmail.com>
2022-01-04 14:46:14 +0100

    actualizado

---
+++ obtener-hash-usuarios.php
@@ -0,0 +1,13 @@
+<?php
+echo password_hash("admin",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+
+<!-- otra forma de crear un hash es de la siguiente forma. una de Alto standing -->
+
+<!-- $password = $_POST['OPERADOR2']; -->
+<!-- $hashed_password = password_hash ($password, PASSWORD_DEFAULT); -->
+<!-- var dump($hashed_password); -->
+
+
+<!-- esto nos sirve para crear un hash (una contraseña encriptada) para la contraseña de nuestro usuario. Lo hacemos con admin, y despues lo repetimos 
+tantas veces, como usuarios queramos crear; en nuestro caso 2 veces mas correspondientes a operador1 y operador 2 -->
